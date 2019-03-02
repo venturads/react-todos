88805061cc199a25f018c0ae6d6738c74f8257ec
venturads <albert@venturads.com>
2019-03-01 20:45:50 -0800

    remove build from ignore git

---
+++ build/precache-manifest.9c291e326cf570ccb1ef66f81d982024.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "a4a2ecca9d7d972ce109",
+    "url": "/static/js/main.a4a2ecca.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "a4a2ecca9d7d972ce109",
+    "url": "/static/css/main.204bcfd5.chunk.css"
+  },
+  {
+    "revision": "71f71c7e859ee3d66a9db5efcbca1036",
+    "url": "/index.html"
+  }
+];
